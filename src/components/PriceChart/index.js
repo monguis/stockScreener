af949c68281886fa7bdd8b8132b5889184f941cd
monguis <jccnnetwork@gmail.com>
2020-10-10 14:10:37 -0600

    added basic chart and api object prototype

--- src/components/PriceChart/index.js
+++ src/components/PriceChart/index.js
@@ -1,40 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Chart from 'chart.js';
-
+import API from "../../utils/API"
 
 const PriceChart = () => {
 
-    const [testState, changeState] = useState([12, 19, 3, 5, 2]);
-    const [lastPrice, changeLastPrice] = useState(6)
+    const [testState, changeState] = useState([]);
     useEffect(() => {
-        const ctx = document.getElementById("myChart");
+        API.testCall().then(({ data }) => {
+            console.log(data)
+            changeState(Object.values(data["Time Series (1min)"]));
+        });
 
+    }, [])
+
+    useEffect(() => {
+
+        var ctx = document.getElementById('myChart');
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [...testState,lastPrice],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
+                labels: testState.map(item => item[ "5. volume"]),
+                    datasets: [{
+                        label: 'Closing Price',
+                        borderColor:'rgba(0, 150, 0, 1)',
+                        data: testState.map(item => item[ "4. close"]),
+                        fill:false,
+                        steppedLine:true,
+                        pointRadius:0,
+                        // backgroundColor: [
+                        //     'rgba(255, 99, 132, 0.2)',
+                        //     'rgba(54, 162, 235, 0.2)',
+                        //     'rgba(255, 206, 86, 0.2)',
+                        //     'rgba(75, 192, 192, 0.2)',
+                        //     'rgba(153, 102, 255, 0.2)',
+                        //     'rgba(255, 159, 64, 0.2)'
+                        // ],
+                        // borderColor: [
+                        //     'rgba(255, 99, 132, 1)',
+                        //     'rgba(54, 162, 235, 1)',
+                        //     'rgba(255, 206, 86, 1)',
+                        //     'rgba(75, 192, 192, 1)',
+                        //     'rgba(153, 102, 255, 1)',
+                        //     'rgba(255, 159, 64, 1)'
+                        // ],
+                        // borderWidth: 3
+                    }]
+    },
             options: {
                 scales: {
                     yAxes: [{
@@ -45,16 +56,11 @@ const PriceChart = () => {
                 }
             }
         });
-    }, [testState,lastPrice])
+    }, [testState])
 
-    const addState = () => {
-        changeLastPrice(lastPrice+1);
-        console.log(lastPrice)
-    }
 
     return (<>
-        <canvas id="myChart" width="400" height="400"> </canvas>
-        <button onClick={addState}>add</button>
+        <canvas id="myChart" styles={{ width: "400px", height: "400px" }}> </canvas>
     </>);
 }
 
--- src/utils/API/index.js
+++ src/utils/API/index.js
@@ -1,8 +1,11 @@
+import axios from "axios";
 const BAS = process.env.REACT_APP_API_KEY;
 
-
 export default {
-    search: function() {
+    testCall: function () {
+        return axios.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=1min&apikey=demo");
+    },
+    search: function () {
         // console.log(BAS);
     }
-  };
+};
